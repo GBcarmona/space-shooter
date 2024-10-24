f3914be54cb1b1d44f77c5ddab1c4c8323e5c477
Gabriel <gbcarmona05@gmail.com>
2024-10-24 19:42:35 -0300

    Add Surface in display surface

--- code/main.py
+++ code/main.py
@@ -2,16 +2,29 @@ import pygame
 #general setup
 pygame.init()
 
+caption = 'Study space gamee'
+pygame.display.set_caption(caption)
 screen = pygame.display.set_mode((1280,720))
 clock = pygame.time.Clock()
 running = True
+
+#testing place surface
+surf = pygame.Surface((100,200))
+surf.fill('orange')
+x= 100
+
+space_ship = pygame.image.load('images/player.png')
+
 while running :
     #pool event
-    #pygame.QUIT = when the user clicks on the X it => closes window
     for event in pygame.event.get() :
         if event.type == pygame.QUIT :
-            running == False
+            running = False
 
-    screen.fill('blue') 
+    screen.fill('silver')
+    screen.blit(surf,(100,150))
+    x+= 0.1
+    screen.blit(space_ship,(100,100))
+    pygame.display.update()
     
 pygame.quit()
